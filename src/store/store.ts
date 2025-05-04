00ede5dd008ea0574f6d68ae3689659faca499ac
Bruno Leonardo Senna <themasteremailsl@gmail.com>
2025-05-04 01:53:54 -0300

    refactor: remove unused comments in store hooks and store configuration

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,7 +3,6 @@ import currencyReducer from './slices/currencySlice';
 import calculatorReducer from './slices/calculatorSlice';
 import resultsReducer from './slices/resultsSlice';
 
-// The store will be updated with actual reducers once slices are created
 export const store = configureStore({
   reducer: {
     currency: currencyReducer,
@@ -12,6 +11,5 @@ export const store = configureStore({
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch; 
